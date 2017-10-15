0086d12e1e5d8f576c72d285ba170162bc831649
Serhiy Dmitrik <dr.voo1646@gmail.com>
2017-10-15 21:02:33 +0300

    Initial commit

---
+++ Patterns/GamingLaptop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    public class GamingLaptop : LaptopBuilder
+    {
+        public override void SetMemory()
+        {
+            Laptop.Memory = "8000MB";
+        }
+
+        public override void SetBattery()
+        {
+            Laptop.Battery = "5000mAp";
+        }
+
+        public override void SetCPU()
+        {
+            Laptop.CPU = "coreI7";
+        }
+
+        public override void SetMonitor()
+        {
+            Laptop.Monitor = "1080p";
+        }
+    }
+}
---
+++ Patterns/SlowLaptop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    public class SlowLaptop : LaptopBuilder
+    {
+        public override void SetMemory()
+        {
+            Laptop.Memory = "2000MB";
+        }
+
+        public override void SetBattery()
+        {
+            Laptop.Battery = "3000mAp";
+        }
+
+        public override void SetCPU()
+        {
+            Laptop.CPU = "coreI3";
+        }
+
+        public override void SetMonitor()
+        {
+            Laptop.Monitor = "720p";
+        }
+    }
+}
