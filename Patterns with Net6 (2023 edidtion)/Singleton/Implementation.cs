f419c8ca454464f4f00c27befe29677b209f8a16
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-01-27 14:34:52 +0200

    add singleton

--- Patterns with Net6 (2023 edidtion)/Singleton/Implementation.cs
+++ Patterns with Net6 (2023 edidtion)/Singleton/Implementation.cs
@@ -1,4 +1,37 @@
 ﻿namespace Singleton
 {
-  
+    ///
+    /// The intent of the singleton is to ensure that a class only has
+    /// one instance, and to provide a global point of accsess to it
+    /// 
+
+    public class Logger {
+
+        private static Logger? _instance;
+
+        public static Logger Instance => _instance ?? (_instance = new Logger());
+
+        protected Logger() { }
+
+        public void Log(string message ) {
+            Console.WriteLine($"Message: {message}");
+        }
+    }
+    
+    
+    public class ThreadSafeLogger {
+
+        // An instance would be constracted when we get this property first time 
+        // in thread safe maner
+        private static readonly Lazy<ThreadSafeLogger> _lazyLogger = 
+            new Lazy<ThreadSafeLogger>(() => new ThreadSafeLogger());
+
+        public static ThreadSafeLogger Instance => _lazyLogger.Value;
+
+        protected ThreadSafeLogger() { }
+
+        public void Log(string message ) {
+            Console.Write($"Message: {message}");
+        }
+    }
 }
--- Patterns with Net6 (2023 edidtion)/Singleton/Program.cs
+++ Patterns with Net6 (2023 edidtion)/Singleton/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance) {
+    Console.WriteLine("same instances");
+}
+
+Logger.Instance.Log("hello world");
+
+
+var instanceTS1 = ThreadSafeLogger.Instance;
+var instanceTS2 = ThreadSafeLogger.Instance;
+
+if (instanceTS1 == instanceTS2 && instanceTS2 == ThreadSafeLogger.Instance) {
+    Console.WriteLine("same thread safe instances");
+}
+
+Logger.Instance.Log("thread safe hello world");
+
+Console.ReadLine();
