eab223a0340298176f04e27628dd37075b0e9351
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-01-27 16:43:58 +0200

    add prototype pattern

--- Patterns with Net6 (2023 edidtion)/Prototype/Implementation.cs
+++ Patterns with Net6 (2023 edidtion)/Prototype/Implementation.cs
@@ -1,4 +1,42 @@
-﻿namespace Prototype
-{
+﻿namespace Prototype;
 
+/// the intent is to specify the kinds of objects to create using a prototypical instance
+/// and create new object by copying this prototype
+///
+/// It allows to copying existing objects without making code dependent on it's concrete classes
+/// Prototype might return shallow or deep copy
+
+// prototype
+public abstract class Person {
+    public abstract string Name { get; set; }
+    public abstract Person Clone();
 }
+
+// concrete prototype1
+public class Manager : Person {
+    public override string Name { get; set; }
+    public Manager(string name) {
+        Name = name;
+    }
+    public override Person Clone() {
+        return (Person)MemberwiseClone();
+    }
+}
+
+public class Employee : Person {
+    public override string Name { get; set; }
+    public Manager Manager { get; set; }
+    public Employee(string name, Manager manager) {
+        Name = name;
+        Manager = manager;
+    }
+    // shallow copy
+    public override Person Clone() {
+        return (Person)MemberwiseClone();
+    }
+}
+
+// concrete prototype2
+
+
+
--- Patterns with Net6 (2023 edidtion)/Prototype/Program.cs
+++ Patterns with Net6 (2023 edidtion)/Prototype/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Prototype";
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+
+var manager = new Manager("Olha");
+var managerClone = manager.Clone();
+
+Console.WriteLine($"Manager was cloned: {managerClone.Name}");
+
+
+var employee = new Employee("Timmy", manager);
+var employeeClone = employee.Clone();
+
+Console.WriteLine($"Employee was cloned: {employeeClone.Name}. " +
+                  $"With manager: {employee.Manager.Name}");
