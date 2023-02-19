76901e808c4638876281102d1a98996ec5f4a96c
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-02-19 18:51:29 +0200

    add Flyweight

--- Patterns with Net6 (2023 edidtion)/Flyweight/Implementation.cs
+++ Patterns with Net6 (2023 edidtion)/Flyweight/Implementation.cs
@@ -3,3 +3,50 @@
 // intent is to use sharing to support large number of fine-grained objects.
 // it shares parts of the state between objects 
 // it is not keep the state in the objects
+
+/// <summary>
+/// Flyweight
+/// </summary>
+public interface ICharacter {
+    void Draw(string fontSize, string fontWeight);
+}
+
+
+public class CharacterA : ICharacter {
+    private char symbol = 'a';
+    public void Draw(string fontSize, string fontWeight) {
+        Console.WriteLine($"Drawing a character '{symbol}' with font size {fontSize} and font weight of {fontWeight}");
+    }
+}
+
+public class CharacterB : ICharacter {
+    private char symbol = 'b';
+    public void Draw(string fontSize, string fontWeight) {
+        Console.WriteLine($"Drawing a character '{symbol}' with font size {fontSize} and font weight of {fontWeight}");
+    }
+}
+
+
+public class CharacterFactory {
+    private readonly Dictionary<char, ICharacter> _characters = new();
+
+    public ICharacter? GetCharacter(char symbol) {
+        if (_characters.ContainsKey(symbol)) {
+            Console.WriteLine("Character reuse");
+            return _characters[symbol];
+        }
+
+        if (symbol == 'a') {
+            _characters.Add('a', new CharacterA());
+            return _characters[symbol];
+        }
+        
+        
+        if (symbol == 'b') {
+            _characters.Add('b', new CharacterB());
+            return _characters[symbol];
+        }
+
+        return null;
+    }
+}
--- Patterns with Net6 (2023 edidtion)/Flyweight/Program.cs
+++ Patterns with Net6 (2023 edidtion)/Flyweight/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.Title = "Flyweight";
+﻿using Flyweight;
+
+Console.Title = "Flyweight";
+
+var testString = "abba";
+
+var factory = new CharacterFactory();
+
+foreach (var symbol in testString) {
+    var character = factory.GetCharacter(symbol);
+    if (character is not null) {
+        character.Draw("12", "600"); // it's something like a cache.
+    }
+}
